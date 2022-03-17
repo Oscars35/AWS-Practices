bd99e2a227d7d44c8e73dac66832798151ec8fe8
Oscar <oscar13785@gmail.com>
2022-03-17 18:00:27 +0100

    Put, get and delete done. Get with some problems

--- practices/practice2/delete.ts
+++ practices/practice2/delete.ts
@@ -2,10 +2,9 @@ import {DynamoDB} from "aws-sdk";
 
 //Si async necessitem retornar una promesa (promise())
 const handler = async(event: any, _context: any) => {
-    //Event is the data, context the function
     //Creating DynamoDB instance
     const client = new DynamoDB.DocumentClient()
-    await client.get({
+    await client.delete({
         TableName: 'oscar-salcedo-dynamodb-table',
         Key: {
             id: event.id
