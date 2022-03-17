83f0005a6c1aee7de30172ecfe30db0e84b38860
Oscar <oscar13785@gmail.com>
2022-03-17 18:14:06 +0100

    Finished practice 2

--- dist/practice2/get.js
+++ dist/practice2/get.js
@@ -4,12 +4,13 @@ exports.handler = void 0;
 const aws_sdk_1 = require("aws-sdk");
 const handler = async (event, _context) => {
     const client = new aws_sdk_1.DynamoDB.DocumentClient();
-    await client.get({
+    let result = await client.get({
         TableName: 'oscar-salcedo-dynamodb-table',
         Key: {
             id: event.id
         }
     }).promise();
+    console.log(result);
 };
 exports.handler = handler;
 //# sourceMappingURL=get.js.map
--- practices/practice2/get.ts
+++ practices/practice2/get.ts
@@ -5,11 +5,13 @@ const handler = async(event: any, _context: any) => {
     //Event is the data, context the function
     //Creating DynamoDB instance
     const client = new DynamoDB.DocumentClient()
-    await client.get({
+    let result = await client.get({
         TableName: 'oscar-salcedo-dynamodb-table',
         Key: {
             id: event.id
         }
     }).promise()
+
+    console.log(result)
 }
 export {handler}
