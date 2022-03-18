2c36d08c7368b27dec41aa46d1161564fb7b158a
Oscar <oscar13785@gmail.com>
2022-03-18 22:31:37 +0100

    Added get to return instead of printing it in console output

--- dist/practice2/get.js
+++ dist/practice2/get.js
@@ -4,13 +4,12 @@ exports.handler = void 0;
 const aws_sdk_1 = require("aws-sdk");
 const handler = async (event, _context) => {
     const client = new aws_sdk_1.DynamoDB.DocumentClient();
-    let result = await client.get({
+    return await client.get({
         TableName: 'oscar-salcedo-dynamodb-table',
         Key: {
             id: event.id
         }
     }).promise();
-    console.log(result);
 };
 exports.handler = handler;
 //# sourceMappingURL=get.js.map
--- practices/practice2/get.ts
+++ practices/practice2/get.ts
@@ -1,17 +1,16 @@
 import {DynamoDB} from "aws-sdk";
+import {DocumentClient} from "aws-sdk/lib/dynamodb/document_client";
 
 //Si async necessitem retornar una promesa (promise())
-const handler = async(event: any, _context: any) => {
+const handler = async(event: any, _context: any) : Promise<DocumentClient.GetItemOutput> => {
     //Event is the data, context the function
     //Creating DynamoDB instance
     const client = new DynamoDB.DocumentClient()
-    let result = await client.get({
+    return await client.get({
         TableName: 'oscar-salcedo-dynamodb-table',
         Key: {
             id: event.id
         }
     }).promise()
-
-    console.log(result)
 }
 export {handler}
