0f38032b4e9ed21404aef8c3345d786c3c52d703
Oscar <oscar13785@gmail.com>
2022-03-25 12:52:39 +0100

    Returning just the tuple in get.ts practice2

--- dist/practice2/get.js
+++ dist/practice2/get.js
@@ -4,12 +4,13 @@ exports.handler = void 0;
 const aws_sdk_1 = require("aws-sdk");
 const handler = async (event, _context) => {
     const client = new aws_sdk_1.DynamoDB.DocumentClient();
-    return await client.get({
+    const response = await client.get({
         TableName: 'oscar-salcedo-dynamodb-table',
         Key: {
             id: event.id
         }
     }).promise();
+    return response.Item;
 };
 exports.handler = handler;
 //# sourceMappingURL=get.js.map
--- practices/practice2/get.ts
+++ practices/practice2/get.ts
@@ -2,15 +2,17 @@ import {DynamoDB} from "aws-sdk";
 import {DocumentClient} from "aws-sdk/lib/dynamodb/document_client";
 
 //Si async necessitem retornar una promesa (promise())
-const handler = async(event: any, _context: any) : Promise<DocumentClient.GetItemOutput> => {
+const handler = async(event: any, _context: any) : Promise<DocumentClient.AttributeMap | undefined> => {
     //Event is the data, context the function
     //Creating DynamoDB instance
     const client = new DynamoDB.DocumentClient()
-    return await client.get({
+    const response = await client.get({
         TableName: 'oscar-salcedo-dynamodb-table',
         Key: {
             id: event.id
         }
     }).promise()
+
+    return response.Item
 }
 export {handler}
